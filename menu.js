13e59b93c170af780947af4aab1925228d89b365
Viadsten <stanulevich.vg@yandex.ru>
2020-12-19 23:32:30 +0300

    add services

---
+++ menu.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $(".wrapper-menu").on('click',function(){
+      $(this).find(".hambergerIcon").toggleClass("open");
+    });
+  })
--- project.js
+++ project.js
@@ -118,8 +118,4 @@ if (mediaQuery960.matches) {
 
 
 
- $(document).ready(function(){
-  $(".wrapper-menu").on('click',function(){
-    $(this).find(".hambergerIcon").toggleClass("open");
-  });
-})
+ 
